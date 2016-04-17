649808a8a62b49abb09f294d115250bc8c8b58ec
Dorota Tomalska <dorota.tomalska@gmail.com>
2016-04-17 14:01:33 +0200

    shop front-end with cart & dummy voucher simulation

--- public/js/controllers/ShopCtrl.js
+++ public/js/controllers/ShopCtrl.js
@@ -1,7 +1,12 @@
 angular.module('ShopCtrl', []).controller('ShopController', function($scope, $http, Voucher, Product, Base64) {
-
     $scope.formData = {};
     $scope.products = {};
+    $scope.cart = [];
+    var cartCounter = 0;
+    $scope.discounts = [];
+    $scope.total = 0;
+    $scope.successMsg = '';
+    $scope.success = false;
     var username = 'user';
     var password = 'passwd';
 
@@ -27,6 +32,50 @@ angular.module('ShopCtrl', []).controller('ShopController', function($scope, $ht
       });
     })();
 
+    function clearCart () {
+      $scope.cart = [];
+      cartCounter = 0;
+      $scope.discounts = [];
+      $scope.total = 0;
+    };
+
+    function showSuccessMsg () {
+      $scope.successMsg = 'Thank you for buying our products!';
+      $scope.success = true;
+    };
+
+    $scope.addToCart = function (name, price) {
+      $scope.cart.push({
+        name: name,
+        price: price,
+        id: cartCounter
+      });
+      cartCounter++;
+      $scope.total += price;
+      $scope.success = false;
+    };
+
+    $scope.removeFromCart = function (id) {
+      var itemToRemove = $scope.cart.filter(function (item) { return item.id === id; })[0];
+      $scope.cart.splice($scope.cart.indexOf(itemToRemove), 1);
+      $scope.total -= itemToRemove.price;
+    };
+
+    $scope.submitVoucher = function () {
+      //console.log($scope.formData);
+      if ($scope.cart.length) {
+        // add dummy voucher for now without checking
+        $scope.discounts.push({
+          value: 10,
+          type: 'EUR'
+        });
+
+        $scope.total -= 10;
+        // TO DO:
+        // verify voucher
+        // if valid - check type & apply to cart (add to discounts and calculate total price)
+      }
+    };
 
     function getVouchers() {
       Voucher.get()
@@ -47,4 +96,14 @@ angular.module('ShopCtrl', []).controller('ShopController', function($scope, $ht
     };
 
     $scope.consumeVoucher = function () {};
+
+    $scope.buyProducts = function () {
+      // TO DO
+      // consume voucher
+
+      // clear products from cart
+      clearCart();
+      // display success message
+      showSuccessMsg();
+    };
 });
