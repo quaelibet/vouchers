f1bb865198b9171e14d4732be208282e0ef00fa9
Dorota Tomalska <dorota.tomalska@gmail.com>
2016-04-17 23:27:32 +0200

    database changed to mlab

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url : 'mongodb://localhost:27017/vouchers'
+  url : 'mongodb://admin:qwe123@ds013250.mlab.com:13250/vouchers'
+  //url : 'mongodb://localhost:27017/vouchers'
 };
